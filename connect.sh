25d3bd12c8afa2a85492f4d4aa15d3abbbeaed72
gregm711 <gregm@wearevest.com>
2018-02-07 13:33:39 -0500

    infra killing it

--- connect.sh
+++ connect.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 export NAME=kops.wallacehatch.com
 export KOPS_STATE_STORE=s3://wallace-hatch-kubernetes
+
+
+
+export KUBE_CA_CERT=$(kubectl config view --flatten --output=json \
+       | jq --raw-output '.clusters[2] .cluster ["certificate-authority-data"]')
+export KUBE_ENDPOINT=$(kubectl config view --flatten --output=json \
+       | jq --raw-output '.clusters[2] .cluster ["server"]')
+export KUBE_ADMIN_CERT=$(kubectl config view --flatten --output=json \
+       | jq --raw-output '.users[2] .user ["client-certificate-data"]')
+export KUBE_ADMIN_KEY=$(kubectl config view --flatten --output=json \
+       | jq --raw-output '.users[2] .user ["client-key-data"]')
+export KUBE_USERNAME=$(kubectl config view --flatten --output=json \
+       | jq --raw-output '.users[2] .user ["username"]')
+
 kops export kubecfg ${NAME}
